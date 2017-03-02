3e7f38fb59d1fba334628e01f58e01d9669033f2
cdragon-1 <streetfood89@gmail.com>
2017-03-03 06:03:04 +0900

    css, templates, html inheritance, one to manyfield

---
+++ learning_site/courses/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+
+class Course(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Step(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    order = models.IntegerField(default=0)
+    course = models.ForeignKey(Course)
+
+    def __str__(self):
+        return self.title
+
+
+
+# c = Course()
+# c.title = "Python Basics"
+# c.description = "Learn the basics of Python"
+# c.save()
+#
+# Course(title="Python Collections", description="Learn about list, dict, and tuple").save()
+#
+# Course.objects.create(title="Object-Oriented Python", description = "Learn about Python's classes")
+#
--- learning_site/learning_site/views.py
+++ learning_site/learning_site/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def hello_world(request):
-    return HttpResponse('Hello Lover!!!')
+    return render(request, 'home.html')
