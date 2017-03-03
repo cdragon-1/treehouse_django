105f58f2bd404853dde001660b34b7bc8f1901a8
cdragon-1 <streetfood89@gmail.com>
2017-03-04 00:27:03 +0900

    course_detail.html 생성 404 error 처리

--- learning_site/courses/models.py
+++ learning_site/courses/models.py
@@ -16,6 +16,10 @@ class Step(models.Model):
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course)
 
+    class Meta:
+        ordering = ['order', ]
+
+
     def __str__(self):
         return self.title
 
--- learning_site/courses/urls.py
+++ learning_site/courses/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
-from .import views
+from . import views
 
 
 
 urlpatterns = [
-    url(r'^$', views.course_list)
+    url(r'^$', views.course_list),
+    url(r'(?P<pk>\d+)/$', views.course_detail),
 ]
 
 
